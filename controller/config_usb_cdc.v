fb33ea79ea9afc066c1c94acc6dbd07f8483aef4
IAmMarcelJung <MarcelJung97@web.de>
2025-03-03 11:04:02 +0100

    feat: try to add a response for when the bitstream was successfully transmitted

--- controller/config_usb_cdc.v
+++ controller/config_usb_cdc.v
@@ -21,6 +21,12 @@ module config_usb_cdc (
 );
 
 
+    localparam DESYNC_FLAG_POS = 20;
+    localparam [31:0] FINISH_FLAG = 32'hFAB0_FABF;
+    reg [2:0] ack_state, ack_state_next;
+    localparam STATE_IDLE = 0,
+        STATE_BYTE_0 = 1, STATE_BYTE_1 = 2, STATE_BYTE_2 = 3, STATE_BYTE_3 = 4;
+
     reg [31:0] word_buffer, write_data;
     reg [1:0] byte_index;
     reg [1:0] byte_index_old;
@@ -29,13 +35,100 @@ module config_usb_cdc (
 
     // TODO: Check if this works
     // currently no data is sent back to the host
-    assign in_valid_o = 1'b0;
-    assign in_data_o = 8'hxx;
+    // assign in_valid_o = 1'b0;
+    // assign in_data_o  = 8'hxx;
+
+    reg in_valid_r, in_valid_next;
+    reg [7:0] in_data_r, in_data_next;
+
+    assign in_valid_o = in_valid_r;
+    assign in_data_o  = in_data_r;
+
+    // always @(posedge clk_i, negedge reset_n_i) begin
+    //     if (!reset_n_i) begin
+    //         in_valid_r <= 1'b0;
+    //         in_data_r  <= 8'b0;
+    //     end else begin
+    //         if (write_data[DESYNC_FLAG_POS]) begin
+    //             in_valid_r <= 1'b1;
+    //             in_data_r  <= FINISH_FLAG;
+    //         end else begin
+    //             // Only send data if the transmission is done
+    //         end
+    //     end
+    // end
+    // TODO: add in_ready_i condition
+
+    always @(*) begin
+        if (!reset_n_i) begin
+            ack_state_next = STATE_IDLE;
+        end else begin
+            ack_state_next = ack_state;
+            case (ack_state)
+                STATE_BYTE_3: if (in_ready_i) ack_state_next = STATE_BYTE_2;
+                STATE_BYTE_2: if (in_ready_i) ack_state_next = STATE_BYTE_1;
+                STATE_BYTE_1: if (in_ready_i) ack_state_next = STATE_BYTE_0;
+                STATE_BYTE_0: if (in_ready_i) ack_state_next = STATE_IDLE;
+                default:
+                if (write_data[DESYNC_FLAG_POS]) begin
+                    if (in_ready_i) ack_state_next = STATE_BYTE_3;
+                end
+            endcase
+        end
+    end
+
+    always @(posedge clk_i, negedge reset_n_i) begin
+        if (!reset_n_i) begin
+            ack_state <= STATE_IDLE;
+        end else begin
+            ack_state <= ack_state_next;
+        end
+    end
+
+    always @(*) begin
+        if (!reset_n_i) begin
+            in_valid_next = 1'b0;
+            in_data_next  = 8'b0;
+        end else begin
+            case (ack_state)
+                STATE_BYTE_3: begin
+                    in_valid_next = 1'b1;
+                    in_data_next  = FINISH_FLAG[24+:8];
+                end
+                STATE_BYTE_2: begin
+                    in_valid_next = 1'b1;
+                    in_data_next  = FINISH_FLAG[16+:8];
+                end
+                STATE_BYTE_1: begin
+                    in_valid_next = 1'b1;
+                    in_data_next  = FINISH_FLAG[8+:8];
+                end
+                STATE_BYTE_0: begin
+                    in_valid_next = 1'b1;
+                    in_data_next  = FINISH_FLAG[0+:8];
+                end
+                default: begin
+                    in_valid_next = 1'b0;
+                    in_data_next  = 8'b0;
+                end
+            endcase
+        end
+    end
+
+    always @(posedge clk_i, negedge reset_n_i) begin
+        if (!reset_n_i) begin
+            in_valid_r <= 1'b0;
+            in_data_r  <= 8'b0;
+        end else begin
+            in_valid_r <= in_valid_next;
+            in_data_r  <= in_data_next;
+        end
+    end
 
     assign word_write_strobe_o = word_write_strobe;
-    assign write_data_o = write_data;
-    assign
-        out_ready_o = 1'b1;  // Fabric is assumed to be clocked high enough that it is always ready
+    assign write_data_o        = write_data;
+    // Fabric is assumed to be clocked fast enough that it is always ready
+    assign out_ready_o         = 1'b1;
 
     always @(posedge clk_i, negedge reset_n_i) begin
         if (!reset_n_i) begin
