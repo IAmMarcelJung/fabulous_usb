6df404423ebe444d970933da3c00f28cc9b337ee
Marcel.Jung <Marcel.Jung@stud.uni-heidelberg.de>
2025-03-14 11:44:27 +0100

    fix: change pll instantiation to 48 and 12 MHz version

--- top_basys3.v
+++ top_basys3.v
@@ -90,13 +90,10 @@ module top_basys3 #(
     // turn off 7 segment display
     assign an                              = {NUM_OF_ANODES{1'b1}};
 
-    pll_48_24_MHz pll_48_24_MHz_int (
+    pll_48_12_MHz pll_48_12_MHz_inst (
         .clk_in1   (clk),         // 100 MHz input clock
         .reset     (reset),       // Reset signal to the clocking wizard
-        .clk_5_MHz (),            // 5 MHz output clock
-        .clk_6_MHz (),            // 6 MHz output clock
         .clk_12_MHz(clk_system),  // 12 MHz output clock
-        .clk_24_MHz(),            // 24 MHz output clock
         .clk_48_MHz(clk_usb),     // 48 MHz output clock
         .locked    (locked)       // Locked output signal
     );
