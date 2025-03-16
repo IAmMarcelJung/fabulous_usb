05906ae248e0977c335aba44eac1fc07739f975f
Marcel.Jung <Marcel.Jung@stud.uni-heidelberg.de>
2025-03-16 19:24:27 +0100

    feat: add an argument parser for the bridge arguments

--- scripts/modules/argument_parser.py
+++ scripts/modules/argument_parser.py
@@ -41,6 +41,36 @@ def parse_arguments():
     )
     return parser.parse_args()
 
+def parse_bridge_arguments():
+    parser = argparse.ArgumentParser(
+        description="Create a server which acts as a bridge between OpenOCD and the JTAG target device can connect."
+    )
+    parser.add_argument(
+        "--acm_port",
+        type=str,
+        default="/dev/ttyACM2",
+        help="The ACM port to for transmitting the JTAG bitbang data, defaults to /dev/ttyACM2.",
+    )
+    parser.add_argument(
+        "--address",
+        type=str,
+        default="127.0.0.1",
+        help="The address of the bridge server, defaults to 127.0.0.1."
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=4567,
+        help="The port of the bridge server, defaults to 4567."
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=False,
+        action="count",
+        help="Show detailed log information including function and line number",
+    )
+    return parser.parse_args()
 
 if __name__ == "__main__":
     pass
